4fd4fd86ef466c14d0c191bcd7d66de785981797
Zeyad Ahmed <zeyadahmed10101@gmail.com>
2024-02-18 19:55:40 +0200

    adding base configs for jpa/hibernte

--- src/main/java/org/zeyad/sms/controllers/StudentController.java
+++ src/main/java/org/zeyad/sms/controllers/StudentController.java
@@ -1,15 +1,21 @@
 package org.zeyad.sms.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.zeyad.sms.entity.Student;
+import org.zeyad.sms.repos.StudentRepository;
 
 @RestController
 @RequestMapping("/api/v1/students")
 public class StudentController {
 
+    @Autowired
+    private StudentRepository studentRepository;
     @GetMapping
     public String getStudents(){
+        studentRepository.save(Student.builder().email("email").name("name").build());
         return "Student";
     }
 }
---
+++ src/main/java/org/zeyad/sms/entity/Student.java
@@ -0,0 +1,33 @@
+package org.zeyad.sms.entity;
+
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    // Many-to-Many relationship with Course
+//    @JsonIgnore
+//    @ManyToMany(mappedBy = "students")
+//    private List<Course> courses = new ArrayList<>();
+
+    // Other attributes like address, phone number, etc.
+}
