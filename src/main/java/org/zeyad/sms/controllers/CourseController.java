3b1757b54e8e1c0312b35e2a8ce8dc46a40e0689
Zeyad Ahmed <zeyadahmed10101@gmail.com>
2024-02-18 23:23:43 +0200

    Adding Course&Techer Controller

---
+++ src/main/java/org/zeyad/sms/controllers/CourseController.java
@@ -0,0 +1,13 @@
+package org.zeyad.sms.controllers;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/courses")
+@Setter
+@Getter
+public class CourseController {
+}
---
+++ src/main/java/org/zeyad/sms/controllers/TeacherController.java
@@ -0,0 +1,13 @@
+package org.zeyad.sms.controllers;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/teachers")
+@Setter
+@Getter
+public class TeacherController {
+}
