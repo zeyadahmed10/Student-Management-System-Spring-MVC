63f4cc60523f3320c8435ea6ec28f18340d0bf88
Zeyad Ahmed <zeyadahmed10101@gmail.com>
2024-02-18 17:55:14 +0200

    addig base configs for SMS projet and spring-mvc

---
+++ src/main/java/org/zeyad/sms/controllers/StudentController.java
@@ -0,0 +1,15 @@
+package org.zeyad.sms.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/students")
+public class StudentController {
+
+    @GetMapping
+    public String getStudents(){
+        return "Student";
+    }
+}
