c4dcad6bb6f0860e20980ed98feaffd7e8504449
Zeyad Ahmed <zeyadahmed10101@gmail.com>
2024-02-19 15:46:45 +0200

    adding global excpetion handler

---
+++ src/main/java/org/zeyad/sms/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package org.zeyad.sms.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpStatusCodeException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = {ResourceNotFoundException.class})
+    protected ResponseEntity<?> handleConflict(ResourceNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
